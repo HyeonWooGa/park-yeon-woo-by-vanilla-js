5acb1405082ae35bca8ac6ac11895d46bdc11606
HyeonWooGa <yeonwoopark22@gmail.com>
2022-07-22 01:43:05 +0900

    GNB 생성 매개, 변수 전달하여 Title 조건문으로 변경

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,20 +1,20 @@
 // 홈페이지 렌더링 콜백 함수
 export const handleHome = (req, res) => {
-  return res.render("home");
+  return res.render("home", { pageTitle: "감사합니다", isNotTitle: false });
 };
 
 export const handleAboutMe = (req, res) => {
-  return res.render("aboutMe");
-};
-
-export const handleEducation = (req, res) => {
-  return res.render("education");
+  return res.render("aboutMe", { pageTitle: "자기 소개", isNotTitle: true });
 };
 
 export const handlePortfolio = (req, res) => {
-  return res.render("portfolio");
+  return res.render("portfolio", { pageTitle: "포트폴리오", isNotTitle: true });
 };
 
 export const handleSkills = (req, res) => {
-  return res.render("skills");
+  return res.render("skills", { pageTitle: "기술 스택", isNotTitle: true });
+};
+
+export const handleEducation = (req, res) => {
+  return res.render("education", { pageTitle: "수료 교육", isNotTitle: true });
 };
